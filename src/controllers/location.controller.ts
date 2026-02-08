8385ed260f9e3122bd7a5860abecabf97b0cfb7c
Pouriya Sedaghat <105992532+pouriya-sedaghat@users.noreply.github.com>
2026-02-08 07:29:16 +0330

    v 1.0.1

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -10,13 +10,12 @@ export const upsertLocation = async (req: Request, res: Response) => {
   try {
     const parse = upsertReqSchema.safeParse(req.body);
 
-    if (!parse.success) {
+    if (!parse.success)
       return res.status(400).json({
         success: false,
         message: "Invalid request body",
         errors: parse.error.format(),
       });
-    }
 
     const { userId, coordinate } = parse.data;
 
@@ -47,19 +46,21 @@ export const upsertLocation = async (req: Request, res: Response) => {
 
 export const findNearby = async (req: Request, res: Response) => {
   try {
-    const parse = findNearbyReqSchema.safeParse(req.body);
+    const params = { ...req.query, ...req.body };
+    const parse = findNearbyReqSchema.safeParse(params);
 
-    if (!parse.success) {
+    if (!parse.success)
       return res.status(400).json({
         success: false,
         message: "Invalid request body",
         errors: parse.error.format(),
       });
-    }
 
-    const { coordinate } = parse.data;
+    const { coordinate, maxDistance, page = 1, pageSize = 5 } = parse.data;
 
-    const nearbyUsers = await Location.aggregate([
+    const skip = (page - 1) * pageSize;
+
+    const result = await Location.aggregate([
       {
         $geoNear: {
           near: {
@@ -68,21 +69,66 @@ export const findNearby = async (req: Request, res: Response) => {
           },
           distanceField: "distance",
           spherical: true,
+          ...(maxDistance ? { maxDistance } : {}),
         },
       },
-      { $limit: 10 },
-      { $project: { userId: 1, distance: 1, updatedAt: 1 } },
+      //   {
+      //     $addFields: {
+      //       score: {
+      //         $add: [
+      //           { $multiply: [-1, "$distance"] }, // closer = higher score
+      //           { $multiply: [1000000, { $toLong: "$updatedAt" }] }, // newer = higher score
+      //         ],
+      //       },
+      //     },
+      //   },
+      {
+        $addFields: {
+          score: {
+            $add: [
+              {
+                $divide: [1, { $add: ["$distance", 1] }],
+              },
+              { $divide: [{ $toLong: "$updatedAt" }, 1e10] },
+            ],
+          },
+        },
+      },
+      { $sort: { score: -1 } }, // sort by combined score
+      {
+        $facet: {
+          data: [
+            { $skip: skip },
+            { $limit: pageSize },
+            {
+              $project: {
+                userId: 1,
+                distance: 1,
+                updatedAt: 1,
+              },
+            },
+          ],
+          totalCount: [{ $count: "count" }],
+        },
+      },
     ]);
 
-    const nearbyUsersWithStatus = nearbyUsers.map((u) => ({
-      ...u,
-      updateStatus: timeAgo(new Date(u.updatedAt)),
-    }));
+    const data = result[0].data;
+    const totalCount = result[0].totalCount[0]?.count ?? 0;
+    const totalPages = Math.ceil(totalCount / pageSize);
+    const hasMore = page < totalPages;
 
     return res.status(200).json({
       success: true,
-      count: nearbyUsers.length,
-      nearbyUsers: nearbyUsersWithStatus,
+      page,
+      pageSize,
+      totalCount,
+      totalPages,
+      hasMore,
+      data: data.map((d: { updatedAt: Date }) => ({
+        ...d,
+        updateStatus: timeAgo(new Date(d.updatedAt)),
+      })),
     });
   } catch (err) {
     console.error("Find Nearby error", err);
--- src/schemas/request.schema.ts
+++ src/schemas/request.schema.ts
@@ -11,4 +11,33 @@ export const upsertReqSchema = z.object({
 
 export const findNearbyReqSchema = z.object({
   coordinate: coordinateSchema,
+  maxDistance: z.coerce
+    .number({
+      invalid_type_error: "Invalid max distance",
+    })
+    .positive({
+      message: "Max distance must be a positive number",
+    })
+    .optional()
+    .refine((v) => v === undefined || !Number.isNaN(v), {
+      message: "Invalid max distance",
+    }),
+  page: z.coerce
+    .number({ invalid_type_error: "Invalid page" })
+    .int({ message: "Page must be an integer" })
+    .positive({ message: "Page must be a positive number" })
+    .default(1),
+  pageSize: z.coerce
+    .number({
+      invalid_type_error: "Invalid limit",
+    })
+    .int({
+      message: "Limit must be an integer",
+    })
+    .positive({
+      message: "Limit must be a positive number",
+    })
+    .min(5, { message: "Limit must be at least 5" })
+    .max(100, { message: "Limit cannot exceed 100" })
+    .default(10),
 });
